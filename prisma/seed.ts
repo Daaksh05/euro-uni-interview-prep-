034430864cb310996db87abaa94d2ddcf9715d11
Daaksh05 <daakshayanidaakshayani@gmail.com>
2026-02-04 19:52:08 +0530

    Update seed script with driver adapter

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,14 @@
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 import fs from 'fs';
 import path from 'path';
 import csv from 'csv-parser';
 
-const prisma = new PrismaClient();
+const connectionString = process.env.POSTGRES_PRISMA_URL || process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 async function readCSV(filePath: string): Promise<any[]> {
     const results: any[] = [];
